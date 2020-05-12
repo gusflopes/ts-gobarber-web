7b62d45577f9dad07e4e9774bf7934d0125f66b9
gusflopes <gusflopes86@gmail.com>
2020-05-11 20:14:29 -0400

    feat: onblur onfocus css

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
@@ -8,13 +9,17 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 export const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
 
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input type="text" icon={FiUser} placeholder="Nome" name="name" />
           <Input type="text" icon={FiMail} placeholder="E-mail" name="email" />
@@ -26,7 +31,7 @@ export const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="home">
           <FiArrowLeft />
